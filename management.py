299546b3214fd51480b929b95236e14ad4721d31
Jalkiran <jalkirankaur36@gmail.com>
2026-06-19 21:47:13 +0530

    Implemented add() method

--- management.py
+++ management.py
@@ -2,3 +2,11 @@ from expense import Expense
 class ExpenseManagement:
     def __init__(self):
         self.expense=[]
+    def add(self):
+        amount=int(input('Enter Amount:'))
+        category=input('Enter Category:').title
+        date=input('Date:')
+        e=Expense(amount,category,date)
+        self.expense.append(e)
+        print('---Expense Added Successfully!---\n')
+    
